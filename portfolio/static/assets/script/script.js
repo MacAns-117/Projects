ae6a381b27289735c2a5f1e494d5fd464a1b2514
Maqsood Ahmed Ansari <106222903+MacAns-117@users.noreply.github.com>
2024-05-23 12:21:06 +0530

    Add files via upload

---
+++ portfolio/static/assets/script/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+      $(this).toggleClass('fa-times');
+      $('header').toggleClass('toggle');
+    });
+  
+    $(window).on('scroll load',function(){
+  
+      $('#menu').removeClass('fa-times');
+      $('header').removeClass('toggle');
+  
+      if($(window).scrollTop() > 0){
+        $('.top').show();
+      }else{
+        $('.top').hide();
+      }
+  
+    });
+  
+    // smooth scrolling 
+  
+    $('a[href*="#"]').on('click',function(e){
+  
+      e.preventDefault();
+  
+      $('html, body').animate({
+  
+        scrollTop : $($(this).attr('href')).offset().top,
+  
+      },
+        500, 
+        'linear'
+      );
+  
+    });
+  
+  });
+
+  // Add this to your existing JavaScript file to hide alerts after a few seconds
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(function() {
+      let alerts = document.querySelectorAll('.alert');
+      alerts.forEach(function(alert) {
+          alert.style.display = 'none';
+      });
+  }, 5000); // Hide after 5 seconds
+});
